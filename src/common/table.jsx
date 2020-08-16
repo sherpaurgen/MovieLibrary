3b2ad8074ea0d7861747fbc502279610b35ccae9
urgen sherpa <7010051+sherpaurgen@users.noreply.github.com>
2020-08-16 22:58:00 +0545

    de-structuring argumentss

--- src/common/table.jsx
+++ src/common/table.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import TableBody from "./TableBody";
 import TableHeader from "../components/TableHeader";
 
-const Table = (props) => {
-  const { columns, sortedColumn, onSort, data } = props;
+const Table = ({ columns, sortedColumn, onSort, data }) => {
+  //this is equivalent to destructure where props is pass as argument
+  //const Table = (props)
+  //const { columns, sortedColumn, onSort, data } = props;
   return (
     <table className="table">
       <TableHeader
